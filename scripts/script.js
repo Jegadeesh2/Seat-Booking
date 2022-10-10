1bde0973a26afacc6c2ebe014a2db3a65c220577
Jegadeesh <sjegadeeshwaran2@gmail.com>
2022-10-10 21:15:41 +0530

    Overall code

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Global variables
+let moviePrice = moviesEl.value;
+let seatCount=0;
+
+//
+const moviesEl=document.querySelector('#movies');
+const countEl=document.querySelector('#count');
+const totalEl=document.querySelector('#total');
+const screenContainerEl=document.querySelector('.screen-container');
+
+
+moviesEl.addEventListener('change', function() {
+  moviePrice = moviesEl.value;
+  countEl.innerText= seatCount;
+  totalEl.innerText= seatCount * moviePrice;
+});
+
+screenContainerEl.addEventListener('click', function (event) {
+  if(event.target.classList.contains('seat')) {
+    if (event.target.classList.contains('occupied')) {
+    }else{
+      event.target.classList.toggle('selected');
+      const seatCountAr = document.querySelectorAll('.row .seat.selected');
+      seatCount =  seatCountAr.length;
+      countEl.innerText = seatCount;
+      totalEl.innerText = seatCount * moviePrice;
+    }
+  }
+});
